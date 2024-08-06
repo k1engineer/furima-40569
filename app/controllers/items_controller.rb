70ea0625d9fcb4a6121a1b392904a974b16c2525
k1engineer <p1.pppk20@gmail.com>
2024-08-07 03:46:44 +0900

    未ログインユーザーはログインページへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
   end
 
@@ -19,6 +22,12 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:title, :detail, :category_id, :condition_id, :shipping_charge_id, :prefecture_id,
-                                 :shipping_day_id, :price).merge(user_id: current_user.id)
+                                 :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    return if user_signed_in?
+
+    redirect_to action: :index
   end
 end
