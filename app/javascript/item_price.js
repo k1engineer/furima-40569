4f6aef1536481155dd378a80beaf033773dfa21e
k1engineer <p1.pppk20@gmail.com>
2024-08-10 16:52:39 +0900

    商品出品機能修正1

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,22 +1,27 @@
-document.addEventListener('DOMContentLoaded', () => {
-   const priceInput = document.getElementById("item-price");
-   const addTaxDom = document.getElementById("add-tax-price");
-   const profitDom = document.getElementById("profit");
- 
-   if (priceInput) {
-     priceInput.addEventListener("input", () => {
-       const inputValue = priceInput.value;
- 
-       // 販売手数料の計算 (10%)
-       const tax = Math.floor(inputValue * 0.1);
- 
-       // 販売利益の計算
-       const profit = Math.floor(inputValue - tax);
- 
-       // 結果をDOMに表示
-       addTaxDom.innerHTML = tax;
-       profitDom.innerHTML = profit;
-     });
-   }
- });
- 
+const price = () => {
+
+  document.addEventListener('turbo:load', () => {
+    const priceInput = document.getElementById("item-price");
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
+  
+    if (priceInput) {
+      priceInput.addEventListener("input", () => {
+        const inputValue = priceInput.value;
+  
+        // 販売手数料の計算 (10%)
+        const tax = Math.floor(inputValue * 0.1);
+  
+        // 販売利益の計算
+        const profit = Math.floor(inputValue - tax);
+  
+        // 結果をDOMに表示
+        addTaxDom.innerHTML = tax;
+        profitDom.innerHTML = profit;
+      });
+    }
+  });
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,8 @@ class Item < ApplicationRecord
   validates :shipping_charge_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validate  :image_presence
 
   has_one_attached :image
