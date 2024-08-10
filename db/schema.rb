4217bbe2b45b371303f66c498605882b3f50eb38
k1engineer <p1.pppk20@gmail.com>
2024-08-10 21:20:33 +0900

    商品出品機能修正3

--- db/schema.rb
+++ db/schema.rb
@@ -74,4 +74,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_04_051805) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "users"
 end
