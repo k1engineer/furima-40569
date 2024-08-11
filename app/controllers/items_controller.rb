d3a998091745e831b6c724dcaaa6665e2e64b3c7
k1engineer <p1.pppk20@gmail.com>
2024-08-11 18:08:40 +0900

    微修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :set_item, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index, :show]
+  before_action :user_verification, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -20,15 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -37,6 +36,10 @@ class ItemsController < ApplicationController
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:title, :detail, :category_id, :condition_id, :shipping_charge_id, :prefecture_id,
@@ -47,5 +50,11 @@ class ItemsController < ApplicationController
     return if user_signed_in?
 
     redirect_to action: :index
+  end
+
+  def user_verification
+    return if current_user == @item.user
+
+    redirect_to root_path
   end
 end
